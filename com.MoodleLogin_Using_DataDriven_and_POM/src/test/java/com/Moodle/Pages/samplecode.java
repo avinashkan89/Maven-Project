682c90156fe2ec2e82dbdc88c5359755eb0534df
Shivanna <Avinash.Shivanna@CenturyLink.com>
2018-07-03 20:58:16 +0530

    this java class has been modified

--- com.MoodleLogin_Using_DataDriven_and_POM/src/test/java/com/Moodle/Pages/samplecode.java
+++ com.MoodleLogin_Using_DataDriven_and_POM/src/test/java/com/Moodle/Pages/samplecode.java
@@ -1,8 +1,12 @@
-public class sample {
+
+package com.Moodle.Pages;
+import org.testng.annotations.Test;
+
+public class samplecode {
 
 @Test
 	public void pullcodefromgit()
 	{
-  System.out.println("samplr print")
+  System.out.println("samplr print");
   }
   }
